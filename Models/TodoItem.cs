5a857829e82defde2aa7cdd4bd4c98ef22913dfd
WillCast <williamisrael210@gmail.com>
2024-11-21 22:59:07 +0100

    test - testing this branch

---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre de la tarea es obligatoria.")]
+        public string Name { get; set; }
+        public bool? IsComplete { get; set; }=false;
+    }
+}
